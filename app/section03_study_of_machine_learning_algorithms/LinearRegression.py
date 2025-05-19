97c28470eb8d4749ea67b40a85f3fb08a1c7d79f
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-19 20:46:10 -0300

    lab linear regression in pythin

---
+++ app/section03_study_of_machine_learning_algorithms/LinearRegression.py
@@ -0,0 +1,41 @@
+# %%
+from numpy import cov, var, sqrt, std, mean, array
+
+# %%
+
+
+class LinearRegression:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.__correlation_coefficient = self.__correlation()
+        self.__inclination = self.__inclination()
+        self.__intercept = self.__intercept()
+
+    def __correlation(self):
+        convariation = cov(self.x, self.y, bias=True)[0][1]
+        variance_x = var(self.x)
+        variance_y = var(self.y)
+        return convariation / sqrt(variance_x * variance_y)
+
+    def __inclination(self):
+        stdx = std(self.x)
+        stdy = std(self.y)
+        return self.__correlation_coefficient * (stdy / stdx)
+
+    def __intercept(self):
+        median_x = mean(self.x)
+        median_y = mean(self.y)
+        return median_y - median_x * self.__inclination
+
+    def predict(self, value):
+        return self.__intercept + (self.__inclination * value)
+
+
+# %%
+x = array([1, 2, 3, 4, 5])
+y = array([2, 4, 6, 8, 10])
+
+lr = LinearRegression(x, y)
+prevision = lr.predict(6)
+print(f"Prevision: {prevision}")
