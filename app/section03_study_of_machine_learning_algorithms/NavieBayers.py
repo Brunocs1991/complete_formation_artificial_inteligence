f454b993468d7b60edc747db988bf2a2e3b1b0d9
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-20 19:46:38 -0300

    implements confusion matrix

--- app/section03_study_of_machine_learning_algorithms/NavieBayers.py
+++ app/section03_study_of_machine_learning_algorithms/NavieBayers.py
@@ -8,24 +8,30 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from yellowbrick.classifier import ConfusionMatrix
+
 # %%
 # import data
 base = pd.read_csv('.data/insurance.csv', keep_default_na=False)
 base.head()
+
 # %%
 # drop unescessary columns
 base = base.drop(columns=['Unnamed: 0'])
 base.head()
+
 # %%
 # discover
 base.shape
+
 # %%
 # check for null values
 print(base.isnull().sum())
+
 # %%
 y = base.iloc[:, 7].values
 X = base.drop(base.columns[7], axis=1).values
 X
+
 # %%
 # transform categorical variables to numerical values using label encoding
 # Note: This is a simple approach and may not be suitable for all datasets
@@ -33,24 +39,30 @@ label_encoder = LabelEncoder()
 for i in range(X.shape[1]):
     if X[:, i].dtype == 'object':
         X[:, i] = label_encoder.fit_transform(X[:, i])
+
 # %%
 # valid data
 X
+
 # %%
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=12)
+
 # %%
 # create the model
 # Note: GaussianNB is a Naive Bayes classifier for Gaussian-distributed data
 model = GaussianNB()
 model.fit(X_train, y_train)
+
 # %%
 # predict
 # Note: The predict method returns the predicted class labels for the input samples
 prediction = model.predict(X_test)
+
 # %%
 # evaluate the model
 prediction
+
 # %%
 accuracy = accuracy_score(y_test, prediction)
 precision = precision_score(y_test, prediction, average='weighted')
@@ -61,7 +73,14 @@ print(f'Accuracy: {accuracy}')
 print(f'Precision: {precision}')
 print(f'Recall: {recall}')
 print(f'F1 Score: {f1}')
+
 # %%
 report = classification_report(y_test, prediction)
 print(report)
+# %%
+cm = ConfusionMatrix(model)
+cm.fit(X_train, y_train)
+cm.score(X_test, y_test)
+cm.show()
+
 # %%
